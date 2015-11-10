892455179f8d503588c15f0d1b0084353f1bd740
Isaac Schaaf <ischaaf@cs.washington.edu>
2015-11-10 12:29:34 -0800

    added reference to MonoGame in Client added BombermanGame to clinet

---
+++ BombermanClient/BombermanGame.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanClient
+{
+    class BombermanGame : Game
+    {
+
+        protected GraphicsDeviceManager graphics;
+        protected SpriteBatch spritebatch;
+
+        public BombermanGame() : base()
+        {
+            
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+        }
+    }
+}
