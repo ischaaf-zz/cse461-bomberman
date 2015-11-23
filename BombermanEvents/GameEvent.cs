8ffe2034f9391db42caeeb151a7e75011735c831
Isaac Schaaf <ischaaf@cs.washington.edu>
2015-11-23 12:22:15 -0800

    ADDED events

---
+++ BombermanEvents/GameEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanEvents
+{
+    public abstract class GameEvent
+    {
+        public EventType Type { get; }
+    }
+}
---
+++ BombermanEvents/MoveEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanEvents
+{
+    public class MoveEvent : GameEvent
+    {
+        public int Player { get; }
+        public Direction Direction { get; }
+
+        public MoveEvent(int player, Direction direction)
+        {
+            Player = player;
+            Direction = direction;
+        }
+    }
+
+    public enum Direction
+    {
+        North, South, East, West
+    }
+}
