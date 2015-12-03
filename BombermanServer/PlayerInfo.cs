ddc54a27bc05d2b27764481b8ba563b5fa491486
Robert Lin <lin1873@cs.washington.edu>
2015-12-03 13:21:16 -0800

    initial server files

---
+++ BombermanServer/PlayerInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanServer
+{
+    public class PlayerInfo
+    {
+        public int PlayerID { get; set; }
+        public int Latency { get; set; }
+    }
+}
---
+++ BombermanServer/ServerBroadcastThread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BombermanServer
+{
+    public class ServerBroadcastThread
+    {
+        public ServerBroadcastThread()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
