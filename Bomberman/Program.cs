b45399ad8c46571a2f3773793f306f788ac7e3b9
Isaac Schaaf <ischaaf@cs.washington.edu>
2015-12-16 14:42:15 -0800

    changed driver

--- Bomberman/Program.cs
+++ Bomberman/Program.cs
@@ -15,25 +15,67 @@ namespace Bomberman
             string host = "";
             int port = -1;
             int players = 4;
+            bool hosting;
+            Process clientProcess = null;
+            Process hostProcess = null;
             Console.WriteLine("Bomberman Game");
+            try {
+                hosting = getHostAndPort(out host, out port, out players);
+            } catch (Exception)
+            {
+                Environment.Exit(-1);
+                return;
+            }
+            bool playAgain = true;
+            while (playAgain)
+            {
+                if (hosting)
+                {
+                    if (hostProcess != null && !hostProcess.HasExited)
+                    {
+                        hostProcess.Kill();
+                    }
+                    hostProcess = Host(port, players, out host);
+                }
+                clientProcess = Play(host, port);
+                Console.WriteLine("-- Begin output from BombermanClient.exe --");
+                while (true)
+                {
+                    Console.WriteLine(clientProcess.StandardOutput.ReadLine());
+                    if (clientProcess.HasExited)
+                        break;
+                }
+                Console.WriteLine("Game Over");
+                Console.Write("Play again? (y/n): ");
+                playAgain = Console.ReadLine().StartsWith("y");
+            }
+        }
+
+        static bool getHostAndPort(out string host, out int port, out int players)
+        {
+            host = "";
+            port = -1;
+            players = 4;
+            bool hosting = false;
             Console.Write("Are you hosting? (y/n): ");
             string resp = Console.ReadLine();
             if (resp.StartsWith("y", true, System.Globalization.CultureInfo.InvariantCulture))
             {
+                hosting = true;
                 Console.Write("Port: ");
                 if (!int.TryParse(Console.ReadLine(), out port))
                 {
                     Console.WriteLine("invalid port number");
-                    return;
+                    throw new Exception();
                 }
                 Console.Write("Number of Players: ");
                 int.TryParse(Console.ReadLine(), out players);
                 if (players < 1 || players > 4)
                 {
                     Console.WriteLine("player range 1-4");
-                    return;
+                    throw new Exception();
                 }
-                Host(port, players, out host);
+                host = "localhost";
             }
             if (host.Length == 0)
             {
@@ -46,15 +88,10 @@ namespace Bomberman
                 if (!int.TryParse(Console.ReadLine(), out port))
                 {
                     Console.WriteLine("invalid port number");
-                    return;
+                    throw new Exception();
                 }
             }
-            var p = Play(host, port);
-            Console.WriteLine("-- Begin output from BombermanClient.exe --");
-            while (true)
-            {
-                Console.WriteLine(p.StandardOutput.ReadLine());
-            }
+            return hosting;
         }
 
         static Process Host(int port, int players, out string host)
